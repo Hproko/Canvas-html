179fc2733967d10668bff27a0f68a628838fc140
Hproko <henriqueprokopenko@hotmail.com>
2022-08-10 20:11:07 -0300

    Trabalho finalizado

--- canvas.js
+++ canvas.js
@@ -8,27 +8,36 @@
 //----------------------------------------------------
 const canvas = document.getElementById("myCanvas")
 const ctx = canvas.getContext('2d')
+const poli = document.getElementById("poli")
+const clear = document.getElementById("limpa")
+const new_reta = document.getElementById("new_color")
 
 let holding_line = 0
 let left = 0
 let right = 0
 let middle = 0
 let reta_travada
-
+let cor_atual = 'black'
 
 //Definicao das classes
 //-----------------------------------------------------
-class reta {
-    constructor (xi, yi, xf, yf) {
+class Reta {
+    constructor (xi, yi, xf, yf, cor_atual) {
         this.xi = xi
         this.yi = yi
         this.xf = xf
         this.yf = yf
+        this.cor = cor_atual
     }
 
     mod_ponta_esq (xi, yi) {
         this.xi = xi
         this.yi = yi
+    }
+
+    mod_ponta_dir (xf, yf) {
+        this.xf = xf
+        this.yf = yf
     }
 
     mod_all (distX, distY) {
@@ -50,34 +59,42 @@ class reta {
         }
     }
 
+    getEquation() {
+
+        let a = (this.yf - this.yi)/(this.xf - this.yi)
+        let b = (this.yf - a * this.xf)
+        return {a , b}
+    }
 }
 
 
 //Declaracao de funcoes 
 //----------------------------------------------------
 function init_canvas (){
-    canvas.width = window.innerWidth / 1.5
-    canvas.height = window.innerHeight / 1.5
+    canvas.width = window.innerWidth / 1.3
+    canvas.height = window.innerHeight / 1.3
 }
 
-function load_line () { 
-    ctx.beginPath()
-    ctx.moveTo(0, 0)
-    ctx.lineTo(500, 200)
-    ctx.strokeStyle = '#ff0000'
-    ctx.lineWidth = 3
-    ctx.stroke()
-}
 
-function draw_line (xi, yi, xf, yf){
+function draw_line (xi, yi, xf, yf, cor){
     ctx.beginPath()
     ctx.moveTo(xi, yi)
     ctx.lineTo(xf, yf)
-    ctx.strokeStyle = '#ff0000'
+    ctx.strokeStyle = cor
     ctx.lineWidth = 3
     ctx.stroke()
 }
 
+function pointIsOnLine(m, c, x, y)
+{
+     let Epsilon = 20
+    // SE (x, y) estiver na reta
+    // vai satistazer a equacao
+    if ((y >= ((m * x) + c) - Epsilon) && (y <= ((m * x) + c) + Epsilon))
+        return true;
+ 
+    return false;
+}
 
 
 function update_canvas () {
@@ -85,7 +102,7 @@ function update_canvas () {
     ctx.clearRect (0, 0, canvas.width, canvas.height) 
 
     for (var i = 0; i < vetor_retas.length; i++){
-        draw_line(vetor_retas[i].xi, vetor_retas[i].yi, vetor_retas[i].xf, vetor_retas[i].yf)
+        draw_line(vetor_retas[i].xi, vetor_retas[i].yi, vetor_retas[i].xf, vetor_retas[i].yf, vetor_retas[i].cor)
     }
 
 }
@@ -98,20 +115,11 @@ const getMousePos = (canvas, evt) => {
     }
 }
 
-function is_on_any_line (x, y) {
+function gruda_mouse (x, y) {
     
 
     for (var i = 0; i < vetor_retas.length; i++){
         
-
-        if ((x >= vetor_retas[i].getMiddle().x - 20 && x <= vetor_retas[i].getMiddle().x + 20) &&
-            (y >= vetor_retas[i].getMiddle().y - 20 && y <= vetor_retas[i].getMiddle().y + 20)){
-                console.log("middle")
-                holding_line = 1
-                reta_travada = i
-                middle = 1
-                break
-        }
 
         if ((x >= vetor_retas[i].xi - 10 && x <= vetor_retas[i].xi + 10) && 
             (y >= vetor_retas[i].yi - 10 && y <= vetor_retas[i].yi + 10)){
@@ -120,18 +128,90 @@ function is_on_any_line (x, y) {
                 left = 1
                 break
         }
+        
+        if ((x >= vetor_retas[i].xf - 10 && x <= vetor_retas[i].xf + 10) && 
+            (y >= vetor_retas[i].yf - 10 && y <= vetor_retas[i].yf + 10)){
+        
+                holding_line = 1
+                reta_travada = i
+                right = 1
+                break
+        }
+
+        if ((x >= vetor_retas[i].getMiddle().x - 20 && x <= vetor_retas[i].getMiddle().x + 20) &&
+            (y >= vetor_retas[i].getMiddle().y - 20 && y <= vetor_retas[i].getMiddle().y + 20)){
+                holding_line = 1
+                reta_travada = i
+                middle = 1
+                break
+        }
+
+
     }
+}
+
+function isOnLine (xp, yp, x1, y1, x2, y2, maxDistance) {
+    var dxL = x2 - x1, dyL = y2 - y1;  // line: vector from (x1,y1) to (x2,y2)
+    var dxP = xp - x1, dyP = yp - y1;  // point: vector from (x1,y1) to (xp,yp)
+
+    var squareLen = dxL * dxL + dyL * dyL;  // squared length of line
+    var dotProd   = dxP * dxL + dyP * dyL;  // squared distance of point from (x1,y1) along line
+    var crossProd = dyP * dxL - dxP * dyL;  // area of parallelogram defined by line and point
+
+    // perpendicular distance of point from line
+    var distance = Math.abs(crossProd) / Math.sqrt(squareLen);
+
+    return (distance <= maxDistance && dotProd >= 0 && dotProd <= squareLen);
+}
+
+function is_on_any_line (x, y) {
+
+    for (var i = 0; i < vetor_retas.length; i++) {
+
+        let Equacao = vetor_retas[i].getEquation()
+
+        if (isOnLine(x, y, vetor_retas[i].xi, vetor_retas[i].yi, vetor_retas[i].xf, vetor_retas[i].yf, 15)){
+
+            vetor_retas.push(new Reta (x, y, vetor_retas[i].xf, vetor_retas[i].yf, vetor_retas[i].cor))
+            vetor_retas.push(new Reta (vetor_retas[i].xi, vetor_retas[i].yi, x, y, vetor_retas[i].cor))
+            vetor_retas.splice(i, 1)
+            break
+
+        }
+    }
+}
+
+function cria_poligono (num_sides) {
+
+    let lado = (2 * Math.PI)/num_sides
+    let xi = canvas.width/2 + 200
+    let yi = canvas.height/2
+    let cor = document.getElementById("cor_poli").value
+
+    for (var i = 0; i < num_sides; i++ ) {
+        
+        let angulo = lado  * (i+1)
+
+        let xf = 200 * Math.cos(angulo) + canvas.width/2
+        let yf = 200 * Math.sin(angulo) + canvas.height/2
+
+        vetor_retas.push(new Reta (xi, yi ,xf, yf, cor))
+
+        xi = xf
+        yi = yf
+    }
+    
 }
 
 
 //Setando event listeners 
 document.addEventListener("click", function (e) {
+    
     let mousePos = getMousePos(canvas, e)
-
 
     if (mousePos.x >= 0 && mousePos.x <= canvas.width && mousePos.y >= 0 &&  mousePos.y <= canvas.height) {
         if (holding_line == 0)
-            is_on_any_line(mousePos.x, mousePos.y)
+            gruda_mouse(mousePos.x, mousePos.y)
         else {
             if (left == 1) {
                 left = 0
@@ -140,6 +220,11 @@ document.addEventListener("click", function (e) {
 
             if (middle == 1) {
                 middle = 0
+                reta_travada = -2
+            }
+
+            if (right == 1){
+                right = 0
                 reta_travada = -2
             }
 
@@ -166,30 +251,59 @@ document.addEventListener("mousemove", function (e){
             let distY = mousePos.y - vetor_retas[reta_travada].getMiddle().y
             vetor_retas[reta_travada].mod_all(distX, distY)
         }
+
+        if (right == 1) {
+            vetor_retas[reta_travada].mod_ponta_dir(mousePos.x, mousePos.y)
+        }
         update_canvas()
     }
 
 })
 
+limpa.addEventListener("click", () => {
+    vetor_retas = []
+    update_canvas()
+})
 
+poli.addEventListener("click", function (e) {
+    let value = document.getElementById("value").value
+    
+    if (value < 3 || value > 8){
+        alert("Numero de lados do poligono invalido")
+        return
+    }
+
+    cria_poligono(value)
+
+    update_canvas()
+})
 
 document.addEventListener("contextmenu", 
-function(e) 
+function (e)  
 {
     e.preventDefault();
+    let mousePos = getMousePos(canvas, e)
 
+    is_on_any_line(mousePos.x, mousePos.y)
 }, false);
     
-
+new_reta.addEventListener("click", () => {
+    cor_atual = document.getElementById("cor_reta").value
+    vetor_retas.push(new Reta (0, 0, canvas.width/2, canvas.height/2, cor_atual))
+    update_canvas()
+})
 
 
 //Execucao principal do script
 //---------------------Inicio----------------------
 init_canvas()
-load_line()
 
 let vetor_retas = []
-vetor_retas.push(new reta (0, 0, 500, 200))
+
+vetor_retas.push(new Reta (0, 0, canvas.width/2, canvas.height/2, cor_atual))
+
+update_canvas()
+
 
 
 
